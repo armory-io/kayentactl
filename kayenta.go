8243f3fc3f68c5a371de7768d0f303fccf43aee7
Isaac Mosquera <imosquer@gmail.com>
2020-12-18 10:43:01 -0500

    adding kayenta module

---
+++ kayenta.go
@@ -0,0 +1,26 @@
+package kayenta
+
+import (
+	"fmt"
+	"time"
+
+	plank "github.com/armory/plank/v3"
+)
+
+type KayentaThing struct {
+	ID   string `json:"id,omitempty" yaml:"id,omitempty" hcl:"id,omitempty"`
+	Type string `json:"type,omitempty" yaml:"type,omitempty" hcl:"type,omitempty"`
+}
+
+func doStuff() {
+
+	fmt.Printf("somestuff")
+	c := plank.New(plank.WithMaxRetries(1), plank.WithRetryIncrement(time.Second))
+	var things []KayentaThing
+	if err := c.Get("https://a5c6ffa5-3028-450c-a1f9-99de7e439480.mock.pstmn.io/users/123", &things); err != nil {
+		fmt.Println("could not get pipelines")
+		fmt.Println(err)
+	} else {
+		fmt.Println("somethings")
+	}
+}
