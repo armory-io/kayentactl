91a4c49c9b90281a54ce0a6f00fad6e5f8940cd2
Ethan Rogers <ethanfrogers@gmail.com>
2021-02-25 14:36:49 -0500

    chore(options): decouple exec request & config
    decouples the execution request from the config. this enables users to
    use a single canary config across multiple executions. the execution
    request (control/experiment group, lifetime, etc) is created from all of
    the CLI args while the canary config is still sourced from disk

--- internal/canaryConfig/provider.go
+++ internal/canaryConfig/provider.go
@@ -14,7 +14,7 @@ import (
 // GetCanaryConfig fetches a canary config from a remote or local source
 // and converts it to a StandaloneCanaryAnalysisInput. It supports both YAML
 // and JSON data formats
-func GetCanaryConfig(location string) (*kayenta.StandaloneCanaryAnalysisInput, error) {
+func GetCanaryConfig(location string) (*kayenta.CanaryConfig, error) {
 	var configProvider func(string) ([]byte, error)
 	if startsWithProtocol(location, []string{"file://", "http://", "https://"}) {
 		configProvider = httpConfigProvider
@@ -27,7 +27,7 @@ func GetCanaryConfig(location string) (*kayenta.StandaloneCanaryAnalysisInput, e
 		return nil, err
 	}
 
-	var input kayenta.StandaloneCanaryAnalysisInput
+	var input kayenta.CanaryConfig
 	if err := parseYamlOrJson(b, &input); err != nil {
 		return nil, fmt.Errorf("failed to deserialize canary config: %w", err)
 	}
